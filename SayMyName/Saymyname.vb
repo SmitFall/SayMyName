5c136ef094df304c323ab23091199b5dca36627d
Smitfall <captainfalconrsmith@gmail.com>
2022-01-19 18:53:25 -0700

    Will add link

--- SayMyName/Saymyname.vb
+++ SayMyName/Saymyname.vb
@@ -2,6 +2,7 @@
 'RCET0265
 'Spring 2022
 'Say My Name
+'https://github.com/SmitFall/SayMyName
 
 
 Option Explicit On
@@ -17,6 +18,7 @@ Module Saymyname
         Console.WriteLine("Please type your name")
         userinput = Console.ReadLine()
         Console.WriteLine("Hello " & userinput & "! aren't you looking nice today!")
+
         'wait for user response
         Console.ReadLine()
 
