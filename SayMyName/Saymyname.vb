dd487f6624d06f20310519b78cf9da5f6069c624
Smitfall <captainfalconrsmith@gmail.com>
2022-01-19 18:50:42 -0700

    Will add console commands

--- SayMyName/Saymyname.vb
+++ SayMyName/Saymyname.vb
@@ -3,11 +3,22 @@
 'Spring 2022
 'Say My Name
 
+
+Option Explicit On
+Option Strict On
+
 Module Saymyname
 
     Sub Main()
         'declare some variables
         Dim userinput As String
+
+
+        Console.WriteLine("Please type your name")
+        userinput = Console.ReadLine()
+        Console.WriteLine("Hello " & userinput & "! aren't you looking nice today!")
+        'wait for user response
+        Console.ReadLine()
 
     End Sub
 
