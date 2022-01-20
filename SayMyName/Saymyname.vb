f34bd9d75320b74a81101c24681df9ad22eb1b15
Smitfall <captainfalconrsmith@gmail.com>
2022-01-19 18:26:39 -0700

    Add project files.

---
+++ SayMyName/Saymyname.vb
@@ -0,0 +1,14 @@
+﻿'Fallon Smith
+'RCET0265
+'Spring 2022
+'Say My Name
+
+Module Saymyname
+
+    Sub Main()
+        'declare some variables
+        Dim userinput As String
+
+    End Sub
+
+End Module
